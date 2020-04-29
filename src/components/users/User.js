2eb3f5db7b4da74763885930702269410cfc0249
Eugene Sokoletsky <esokoletsky@gmail.com>
2020-04-29 19:56:03 -0400

    heroku deploy

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,8 +1,8 @@
 import React, { Fragment, Component } from "react";
+import { Link } from "react-router-dom";
 import Spinner from "../layout/Spinner";
 import Repos from "../repos/Repos";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
 
 class User extends Component {
   componentDidMount() {
